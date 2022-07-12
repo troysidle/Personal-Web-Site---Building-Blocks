adcd133ce377f1de6e913262c84cfb02bf4b4c61
Troy Sidle <107270605+troysidle@users.noreply.github.com>
2022-07-11 23:31:06 -0400

    Slideshow Start
    This is a five photo slideshow. The bulk of which is taken from w3schools.com.
    
    The main adjustment is getting irregularly sized photos to be vertically centered.

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// function to set a given theme/color-scheme
-        function setTheme(themeName) {
-            localStorage.setItem('theme', themeName);
-            document.documentElement.className = themeName;
-        }
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-        // Immediately invoked function to set the theme on initial load
-        (function () 
-            {
-                setTheme('monochromeGreen');
-            }
-        )();
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-
-window.onscroll = function() {navigationWidth()};
-
-function navigationWidth() {
-  if (document.documentElement.scrollTop > 50) {
-    
-    document.getElementById("sideNavigation").style.width = "180px";  } else {
-        document.getElementById("sideNavigation").style.width = "215px";
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
