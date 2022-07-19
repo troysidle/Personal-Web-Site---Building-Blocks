da1135378e597ed0f3be2b9d016449a263a02fb1
Troy Sidle <107270605+troysidle@users.noreply.github.com>
2022-07-18 23:20:30 -0400

    PopUp Window
    There is a pop up window that is functional for one book. The rest will be updated with the same functionality.

--- main.js
+++ main.js
@@ -22,3 +22,49 @@ function navigationWidth() {
         document.getElementById("sideNavigation").style.width = "215px";
   }
 }
+
+// for window pup up in Publications
+
+// Get the modal
+var firstModal = document.getElementById("firstBookModal");
+var secondModal = document.getElementById("secondBookModal");
+
+// Get the button that opens the modal
+var button1 = document.getElementById("firstBook");
+var button2 = document.getElementById("secondBook");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementById("firstBookModal").getElementsByClassName("selectClose") [0];
+var span2 = document.getElementById("secondBookModal").getElementsByClassName("selectClose") [0];
+
+// When the user clicks the button, open the modal 
+
+button1.onclick = function() {
+  firstModal.style.display = "block";
+}
+
+button2.onclick = function() {
+  secondModal.style.display = "block";
+}
+
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  firstModal.style.display = "none";
+}
+
+span2.onclick = function() {
+  secondModal.style.display = "none";
+}
+
+window.addEventListener("click", function(event) {
+  if (event.target == firstModal) {
+    firstModal.style.display = "none";
+  }
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == secondModal) {
+    secondModal.style.display = "none";
+  }
+});
