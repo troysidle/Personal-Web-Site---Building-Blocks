9d485fea090878860c0b4ca31d2aecdfc88d6c3e
Troy Sidle <107270605+troysidle@users.noreply.github.com>
2022-07-19 22:00:06 -0400

    Pop-Up Windows for Publications
    Name: Building Blocks - v1.4.5
    Author: Troy Sidle
    
    This site is currently under construction. With more content added, the site will be finalized at its basic level.
    
    The color scheme change now works, allowing a user to change the color scheme of the entire page. 
    
    Tooltips and a flexbox are now added to Color Scheme Changes and other minor changes.
    
    There are pop up windows for each book.

--- main.js
+++ main.js
@@ -28,14 +28,26 @@ function navigationWidth() {
 // Get the modal
 var firstModal = document.getElementById("firstBookModal");
 var secondModal = document.getElementById("secondBookModal");
+var thirdModal = document.getElementById("thirdBookModal");
+var fourthModal = document.getElementById("fourthBookModal");
+var fifthModal = document.getElementById("fifthBookModal");
+var sixthModal = document.getElementById("sixthBookModal");
 
 // Get the button that opens the modal
 var button1 = document.getElementById("firstBook");
 var button2 = document.getElementById("secondBook");
+var button3 = document.getElementById("thirdBook");
+var button4 = document.getElementById("fourthBook");
+var button5 = document.getElementById("fifthBook");
+var button6 = document.getElementById("sixthBook");
 
 // Get the <span> element that closes the modal
 var span1 = document.getElementById("firstBookModal").getElementsByClassName("selectClose") [0];
 var span2 = document.getElementById("secondBookModal").getElementsByClassName("selectClose") [0];
+var span3 = document.getElementById("thirdBookModal").getElementsByClassName("selectClose") [0];
+var span4 = document.getElementById("fourthBookModal").getElementsByClassName("selectClose") [0];
+var span5 = document.getElementById("fifthBookModal").getElementsByClassName("selectClose") [0];
+var span6 = document.getElementById("sixthBookModal").getElementsByClassName("selectClose") [0];
 
 // When the user clicks the button, open the modal 
 
@@ -45,6 +57,23 @@ button1.onclick = function() {
 
 button2.onclick = function() {
   secondModal.style.display = "block";
+}
+
+button3.onclick = function() {
+  thirdModal.style.display = "block";
+}
+
+button4.onclick = function() {
+  fourthModal.style.display = "block";
+    
+}
+
+button5.onclick = function() {
+  fifthModal.style.display = "block";
+}
+
+button6.onclick = function() {
+  sixthModal.style.display = "block";
 }
 
 
@@ -57,6 +86,27 @@ span2.onclick = function() {
   secondModal.style.display = "none";
 }
 
+span3.onclick = function() {
+  thirdModal.style.display = "none";
+}
+
+span4.onclick = function() {
+  fourthModal.style.display = "none";
+}
+
+span5.onclick = function() {
+  fifthModal.style.display = "none";
+}
+
+span6.onclick = function() {
+  sixthModal.style.display = "none";
+}
+
+
+
+// Convert this to a switch statement?
+
+
 window.addEventListener("click", function(event) {
   if (event.target == firstModal) {
     firstModal.style.display = "none";
@@ -66,5 +116,29 @@ window.addEventListener("click", function(event) {
 window.addEventListener("click", function(event) {
   if (event.target == secondModal) {
     secondModal.style.display = "none";
+  }
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == thirdModal) {
+    thirdModal.style.display = "none";
+  }
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == fourthModal) {
+    fourthModal.style.display = "none";
+  }
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == fifthModal) {
+    fifthModal.style.display = "none";
+  }
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == sixthModal) {
+    sixthModal.style.display = "none";
   }
 });
