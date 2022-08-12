24d3b0289f5534f32d04653b37f1e98bddb7ada1
Troy Sidle <107270605+troysidle@users.noreply.github.com>
2022-08-12 14:36:16 -0400

    More Functionality for Mobile
    This page uses Bootstrap.
    
    This version has a home image as a background with text.
    
    This scales well on a mobile device. It has a menu that is hidden, but can be displayed with a menu button.
    
    Google Font Montserrat has been added, bold is enabled, more format adjustments.
    
    Custom colors and other design elements added.
    
    Added icons for social media.
    
    Added more functionality for mobile devices - close navigation bar when clicking off of it

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // function to set a given theme/color-scheme
         function setTheme(themeName) {
             localStorage.setItem('theme', themeName);
@@ -13,142 +12,74 @@
         )();
 
 
-window.onscroll = function() {navigationWidth()};
+/* This is edited code adopted from BootstrapMade.com. */
 
-function navigationWidth() {
-  if (document.documentElement.scrollTop > 50) {
+
+(function () {"use strict"; /*all variables must be declared*/
+
+              
+  /* Easy selector helper function defines select */
+  /* This function seems to indicate which elements have classes that have been selected. */
     
-    document.getElementById("navigationBar").style.width = "180px";  } else {
-        document.getElementById("navigationBar").style.width = "215px";
+ const select = function (elements, all = false)
+ {
+    elements = elements.trim() /*trim white space from a string*/
+    if (all) {
+      return [...document.querySelectorAll(elements)]
+    } else {
+      return document.querySelector(elements)
+    }
   }
-}
 
-
-function navigationMenuToggle() {
-  var x = document.getElementById("navigationBar");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+  /* Easy event listener function defines the on function */
+  /* This function seems to indicate which elements have classes that are activated. */
+ 
+  const on = function (type, elements, listener, all = false) 
+  {
+    let selectElement = select (elements, all)
+    if (selectElement) {
+      if (all) {
+        selectElement.forEach(eventAction => eventAction.addEventListener(type, listener)) /*parse an array*/
+      } else {
+        selectElement.addEventListener(type, listener)
+      }
+    }
   }
-}
 
-// for window pup up in Publications
 
+  
+  /**
+   * Mobile navigation toggle
+   */
+  on('click', '.mobileNavigationToggleButton', function(e) {
+    select('body').classList.toggle('mobile-nav-active') /* give the body an attribute that the mobile navigation menu is showing */
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+    
+    
+    
+  /**
+   * Hide Menu Button - new
+  */
+   
+  on('click', '.hideMenuArea', function(e) {
+      let body = select('body')
+      if (body.classList.contains('mobile-nav-active')) /* if the mobile navigation menu is showing */{
+        body.classList.remove('mobile-nav-active') /*stop showing the mobile navigation menu*/
+        let navbarToggle = select('.mobileNavigationToggleButton') /* the button for toggling the navigation menu */
+        navbarToggle.classList.toggle('bi-list') /* show three bars icon */
+        navbarToggle.classList.toggle('bi-x') /* show the X icon*/
+      }
+  })              
+              
+/*toggle off the sliding navigation bar when user clicks outside of it*/
+ /*in process*/
+              
 // Get the modal
-var firstModal = document.getElementById("firstBookModal");
-var secondModal = document.getElementById("secondBookModal");
-var thirdModal = document.getElementById("thirdBookModal");
-var fourthModal = document.getElementById("fourthBookModal");
-var fifthModal = document.getElementById("fifthBookModal");
-var sixthModal = document.getElementById("sixthBookModal");
-
-// Get the button that opens the modal
-var button1 = document.getElementById("firstBook");
-var button2 = document.getElementById("secondBook");
-var button3 = document.getElementById("thirdBook");
-var button4 = document.getElementById("fourthBook");
-var button5 = document.getElementById("fifthBook");
-var button6 = document.getElementById("sixthBook");
-
-// Get the <span> element that closes the modal
-var span1 = document.getElementById("firstBookModal").getElementsByClassName("selectClose") [0];
-var span2 = document.getElementById("secondBookModal").getElementsByClassName("selectClose") [0];
-var span3 = document.getElementById("thirdBookModal").getElementsByClassName("selectClose") [0];
-var span4 = document.getElementById("fourthBookModal").getElementsByClassName("selectClose") [0];
-var span5 = document.getElementById("fifthBookModal").getElementsByClassName("selectClose") [0];
-var span6 = document.getElementById("sixthBookModal").getElementsByClassName("selectClose") [0];
-
-// When the user clicks the button, open the modal 
-
-button1.onclick = function() {
-  firstModal.style.display = "block";
-}
-
-button2.onclick = function() {
-  secondModal.style.display = "block";
-}
-
-button3.onclick = function() {
-  thirdModal.style.display = "block";
-}
-
-button4.onclick = function() {
-  fourthModal.style.display = "block";
-    
-}
-
-button5.onclick = function() {
-  fifthModal.style.display = "block";
-}
-
-button6.onclick = function() {
-  sixthModal.style.display = "block";
-}
-
-
-// When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
-  firstModal.style.display = "none";
-}
-
-span2.onclick = function() {
-  secondModal.style.display = "none";
-}
-
-span3.onclick = function() {
-  thirdModal.style.display = "none";
-}
-
-span4.onclick = function() {
-  fourthModal.style.display = "none";
-}
-
-span5.onclick = function() {
-  fifthModal.style.display = "none";
-}
-
-span6.onclick = function() {
-  sixthModal.style.display = "none";
-}
 
 
 
-// Convert this to a switch statement?
+  
 
-
-window.addEventListener("click", function(event) {
-  if (event.target == firstModal) {
-    firstModal.style.display = "none";
-  }
-});
-
-window.addEventListener("click", function(event) {
-  if (event.target == secondModal) {
-    secondModal.style.display = "none";
-  }
-});
-
-window.addEventListener("click", function(event) {
-  if (event.target == thirdModal) {
-    thirdModal.style.display = "none";
-  }
-});
-
-window.addEventListener("click", function(event) {
-  if (event.target == fourthModal) {
-    fourthModal.style.display = "none";
-  }
-});
-
-window.addEventListener("click", function(event) {
-  if (event.target == fifthModal) {
-    fifthModal.style.display = "none";
-  }
-});
-
-window.addEventListener("click", function(event) {
-  if (event.target == sixthModal) {
-    sixthModal.style.display = "none";
-  }
-});
+})()
